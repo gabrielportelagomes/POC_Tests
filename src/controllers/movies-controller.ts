9c1d50f52e7fcf617a9ed2b1c1ce7cd04e3c945d
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-02-06 11:28:41 -0300

    refactor: status code

--- src/controllers/movies-controller.ts
+++ src/controllers/movies-controller.ts
@@ -16,10 +16,10 @@ export async function postMovie(req: Request, res: Response) {
     return res.sendStatus(201);
   } catch (error) {
     if (error.name === "DuplicatedItem") {
-      return res.status(500).send(error.message);
+      return res.status(409).send(error.message);
     }
     if (error.name === "NotFoundError") {
-      return res.status(500).send(error.message);
+      return res.status(404).send(error.message);
     }
     return res.sendStatus(500);
   }
